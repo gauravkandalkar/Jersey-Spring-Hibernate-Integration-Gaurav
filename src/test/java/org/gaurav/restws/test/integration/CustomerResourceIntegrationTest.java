2f0dbe0b18a283b75a782bd2dfc74a7613fd880d
gauravkandalkar <gakandalkar@gmail.com>
2015-12-27 22:06:03 +0000

    added more integration test cases

--- src/test/java/org/gaurav/restws/test/integration/CustomerResourceIntegrationTest.java
+++ src/test/java/org/gaurav/restws/test/integration/CustomerResourceIntegrationTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.*;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.gaurav.restws.vo.Customers;
@@ -14,6 +16,8 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.sun.jersey.api.client.ClientResponse.Status;
 
 public class CustomerResourceIntegrationTest {
 
@@ -32,11 +36,10 @@ public class CustomerResourceIntegrationTest {
 		client.close();
 	}
 
-
 	@Before
 	public void setUp() throws Exception {
 		target = client.target(Resource_URI).path("Jersey-Spring-Integration/customers");
-		target1 = target.path("/{id}").resolveTemplate("id", 1);
+		target1 = target.path("/{id}");
 	}
 
 	@After
@@ -44,24 +47,62 @@ public class CustomerResourceIntegrationTest {
 	}
 
 	@Test
+	public void checkCustomersNoResourceFound()
+	{
+	 WebTarget targetNothing = client.target(Resource_URI);
+	 Response response = targetNothing.request().get();
+	 assertEquals(404, response.getStatus());
+	}
+	
+	@Test
 	public void checkGetAllCustomersResponseOK() {
-	Response response = target.request().get();
+		Response response = target.request().get();
 		assertEquals(200, response.getStatus());
 	}
 
 	@Test
-	public void checkGetOneCustomerResponseOK()
-	{
+	public void checkGetOneCustomerResponseOK() {
+		target1 = target1.resolveTemplate("id", 5);
 		Response response = target1.request().get();
 		assertEquals(200, response.getStatus());
 	}
+	
+	@Test
+	public void checkGetOneCustomerResponseNot_Found() {
+		target1 = target1.resolveTemplate("id", 3);
+		Response response = target1.request().get();
+		assertEquals(500, response.getStatus());
+	}
 
 	@Test
-	public void checkGetOneCustomerResponseContents()
-	{
-		Customers responseCust  = target1.request().get(Customers.class);
-		assertEquals(1, responseCust.getCustomerID());
-		assertEquals("Gaurav", responseCust.getFirstName());
+	public void checkGetOneCustomerResponseContents() {
+		target1 = target1.resolveTemplate("id", 2);
+		Customers responseCust = target1.request().get(Customers.class);
+		assertEquals(2, responseCust.getCustomerID());
+		assertEquals("Anu", responseCust.getFirstName());
 		assertEquals("Kandalkar", responseCust.getLastName());
 	}
+
+	@Test
+	public void checkPostCustomerResponse() {
+		Customers newCust = new Customers();
+		newCust.setFirstName("Vedant");
+		newCust.setLastName("Ambadkar");
+		Customers responsecustomers = target.request().post(Entity.entity(newCust, MediaType.APPLICATION_JSON),
+				Customers.class);
+		assertNotNull(responsecustomers.getCustomerID());
+
+	}
+
+	@Test
+	public void checkPutCustomerResponse() {
+		Customers newCust = new Customers();
+		newCust.setCustomerID(1);
+		newCust.setFirstName("Vedant");
+		newCust.setLastName("Ambadkar");
+		Customers responsecustomers = target.request().put(Entity.entity(newCust, MediaType.APPLICATION_JSON),
+				Customers.class);
+		assertEquals(newCust.getFirstName(), responsecustomers.getFirstName());
+	}
+
 }
